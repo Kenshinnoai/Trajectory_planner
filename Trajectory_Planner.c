c27aa2d9411aca8d2f831c8ae4e7f479c1a667d9
Timofey Goretskiy <69255946+Kenshinnoai@users.noreply.github.com>
2024-04-01 22:23:39 +0300

    added the function of entering values through console, put all values to struct and separated all functions

--- Trajectory_Planner.c
+++ Trajectory_Planner.c
@@ -1,76 +1,78 @@
 #include <stdio.h>
 #include <math.h>
 #include <locale.h>
+#include <stdlib.h>
 
-static int dispValues(double T, double x0, double v0, double x1, double v1)
+struct Spline
 {
-            /* объявление переменных для дополнения данных */
+    double x0, v0, x1, v1, DestTime, rate;
+    double a0, a1, a2, a3;
+};
 
-    double rate;
-    double T1 = 0;
+static void CoeffCount(struct Spline spl);
 
-            /* объявлене и инициализация необходимых констаант */
+static void DispValues(struct Spline spl);
 
-    const double a0 = x0;
-    const double a1 = v0;
-    const double a2 = (3 * (x1 - x0) + T * (2 * v0 - v1)) / pow(T, 2);
-    const double a3 = (T * (v1 - 3 * v0) - 2 * (x1 - x0)) / pow(T, 3);
 
-            /* инициализация интервалов вывода */
-            
-    printf("Введите значение интервала времени, через который будут выводиться данные (прим. 1 (сек), 0.5 (сек) и тд)\t");
-
-    if (!scanf("%lf", &rate))
-        return 1;
-
-            /* далее вывод данных по rate секундам */
-    
-    printf("время        x0     v0,     x1,     v1\n");
-    
-    for (; T1 <= T; T1 += rate)
-    {
-        printf("\n%.3lf сек %8.3lf, %4.3lf, %4.3lf, %4.3lf\n", T1, x0, v0, x1, v1);
-        
-        x1 = a0 + a1 * T1 + a2 * pow(T1, 2) + a3 * pow(T1, 3);
-        v1 = a1 + 2 * a2 * T1 + 3 * a3 * pow(T1, 2);
-    }
-}
-
-int main()
+int main(int argc, char* argv[])
 {
-            /* Объявление переменных для хранения начальных значений */
-
     setlocale(LC_ALL, "Rus");
-    double x0, v0, x1, v1;
-    double T = 0;
 
-            /* инициализация начальных значений переменных */
+    /* Проверка на достаточное количество аргументов */
 
-    printf("Введите начальные и конечные данные графика\n\n");
+    if (argc < 7)
+    {
+        printf("Введено недостаточно аргументов\n");
 
-    printf("Сначала начальное положение:\t");
-    if (!scanf("%lf", &x0))
         return 1;
+    }
 
-    printf("Теперь начальную скорость:\t");
-    if(!scanf("%lf", &v0))
-        return 1;
+    /* Объявление структуры для хранения введённых значений */
 
-    printf("\nА теперь конечное положение:\t");
-    if(!scanf("%lf", &x1))
-        return 1;
+    struct Spline s1;
 
-    printf("Теперь конечную скорость:\t");
-    if(!scanf("%lf", &v1))
-        return 1;
+    s1.x0 = atof(argv[1]);
+    s1.v0 = atof(argv[2]);
 
-    printf("А тут время конца движения:\t");
-    if(!scanf("%lf", &T))
-        return 1;
+    s1.x1 = atof(argv[3]);
+    s1.v1 = atof(argv[4]);
 
-            /* передача аргументов функции для их последующего отображения */
+    s1.DestTime = atof(argv[5]);
+    s1.rate = atof(argv[6]);
 
-    dispValues(T, x0, v0, x1, v1);
+    /* Подсчёт коэффициентов уравнения */
+
+    CoeffCount(s1);
+    
+    /* Передача аргументов функции для их последующего отображения */
+
+    DispValues(s1);
 
     return 0;
+}
+
+static void CoeffCount(struct Spline spl)
+{
+    /* Подсчёт коэффициентов с помощью введённых данных */
+
+    spl.a0 = spl.x0;
+    spl.a1 = spl.v0;
+    spl.a2 = (3 * (spl.x1 - spl.x0) + spl.DestTime * (2 * spl.v0 - spl.v1)) / pow(spl.DestTime, 2);
+    spl.a3 = (spl.DestTime * (spl.v1 - 3 * spl.v0) - 2 * (spl.x1 - spl.x0)) / pow(spl.DestTime, 3);
+}
+
+static void DispValues(struct Spline spl)
+{
+    /* Далее вывод данных по rate секундам */
+
+    printf("время        x0     v0,     x1,     v1\n");
+
+    for (float T = 0; T <= spl.DestTime; T += spl.rate)
+    {
+        printf("\n%.3f сек %8.3lf, %4.3lf, %4.3lf, %4.3lf\n", T, spl.x0, spl.v0, spl.x1, spl.v1);
+
+        spl.x1 = spl.a0 + spl.a1 * T + spl.a2 * pow(T, 2) + spl.a3 * pow(T, 3);
+        spl.v1 = spl.a1 + 2 * spl.a2 * T + 3 * spl.a3 * pow(T, 2);
+    }
+
 }
